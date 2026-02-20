2cdb8c10d8f141c46f8e159ffe75e1dc9b4c954c
salhalblwshy85-ui <salhalblwshy85@gmail.com>
2026-02-20 03:32:10 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <div style={{textAlign:"center",marginTop:"50px"}}>
+      <h1>مرحبا بكم في متجر شرنقة</h1>
+      <p>🎉 الموقع شغال 🎉</p>
+    </div>
+  );
+}
